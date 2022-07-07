a0500621f2cedbd8f14eecfba4e1f25a45f7b597
khoufstudio <rizqy.nurhaqy@gmail.com>
2022-07-07 21:15:49 +0700

    pagination based on total page

--- src/components/SimplePagination.vue
+++ src/components/SimplePagination.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const count = ref(0)
+const totalPage = ref<number>(9)
+
 </script>
 
 <template>
-  <p class="font-bold ">Hello</p>
+  <div class="flex gap-2 justify-center">
+    <!-- first button -->
+    <button v-if="totalPage > 0">1</button>
+
+    <!-- rest buttons  -->
+    <div v-for="x in totalPage" :key="x">
+      <button v-if="x > 1 && x < totalPage">{{ x }}</button>
+    </div>
+
+    <!-- last button -->
+    <button>{{ totalPage }}</button>
+  </div>
+
+  <!-- input container -->
+  <div class="mt-4">
+    <!-- total page -->
+    <legend>
+      <label  class="mr-2" for="totalPage">total page</label>
+      <input class="border border-slate-200" type="number" v-model="totalPage">
+    </legend>
+  </div>
 </template>
